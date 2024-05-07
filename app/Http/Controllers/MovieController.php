7ee68d3b8022960df3aad2f4eaf797cc1d271657
Chi Nghia <nghia520414@gmail.com>
2024-05-07 21:53:06 +0700

    add-new-film

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -4,6 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Movie;
+use App\Models\Category;
+use App\Models\Genre;
+use App\Models\Country;
+
+
 class MovieController extends Controller
 {
     /**
@@ -19,7 +25,11 @@ class MovieController extends Controller
      */
     public function create()
     {
-        //
+        $category = Category::pluck('title', 'id');
+        $genre = Genre::pluck('title', 'id');
+        $country = Country::pluck('title', 'id');
+        $list = Movie::with('category', 'genre', 'country')->orderBy('id', 'DESC')->get();
+        return view('admin.movie.form', compact('category', 'genre', 'country', 'list'));
     }
 
     /**
@@ -27,7 +37,28 @@ class MovieController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->all();
+        $movie = new Movie();
+        $movie->title = $data['title'];
+        $movie->slug = $data['slug'];
+        $movie->description = $data['description'];
+        $movie->status = $data['status'];
+        $movie->category_id = $data['category_id'];
+        $movie->genre_id = $data['genre_id'];
+        $movie->country_id = $data['country_id'];
+        //thêm hình ảnh
+        $get_image = $request->file('image');
+
+
+        if($get_image){
+            $get_name_image = $get_image->getClientOriginalName();
+            $name_image = current(explode('.', $get_name_image));
+            $new_image = $name_image.rand(0,9999).'.'.$get_image->getClientOriginalExtension();
+            $get_image->move('uploads/movie/', $new_image);
+            $movie->image = $new_image;
+        }
+        $movie->save();
+        return redirect()->back();
     }
 
     /**
@@ -35,7 +66,28 @@ class MovieController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $data = $request->all();
+        $movie = new Movie();
+        $movie->title = $data['title'];
+        $movie->slug = $data['slug'];
+        $movie->description = $data['description'];
+        $movie->status = $data['status'];
+        $movie->category_id = $data['category_id'];
+        $movie->genre_id = $data['genre_id'];
+        $movie->country_id = $data['country_id'];
+        //thêm hình ảnh
+        $get_image = $request->file('image');
+
+
+        if($get_image){
+            $get_name_image = $get_image->getClientOriginalName();
+            $name_image = current(explode('.', $get_name_image));
+            $new_image = $name_image.rand(0,9999).'.'.$get_image->getClientOriginalExtension();
+            $get_image->move('uploads/movie/', $new_image);
+            $movie->image = $new_image;
+        }
+        $movie->save();
+        return redirect()->back();
     }
 
     /**
@@ -43,7 +95,12 @@ class MovieController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $category = Category::pluck('title', 'id');
+        $genre = Genre::pluck('title', 'id');
+        $country = Country::pluck('title', 'id');
+        $list = Movie::with('category', 'genre', 'country')->orderBy('id', 'DESC')->get();
+        $movie = Movie::find($id);
+        return view('admin.movie.form', compact('category', 'genre', 'country', 'list', 'movie'));
     }
 
     /**
@@ -51,7 +108,32 @@ class MovieController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $data = $request->all();
+
+        $movie =  Movie::find($id);
+        $movie->title = $data['title'];
+        $movie->slug = $data['slug'];
+        $movie->description = $data['description'];
+        $movie->status = $data['status'];
+        $movie->category_id = $data['category_id'];
+        $movie->genre_id = $data['genre_id'];
+        $movie->country_id = $data['country_id'];
+        
+        //thêm hình ảnh
+        $get_image = $request->file('image');
+
+        if($get_image){
+            if(!empty($movie->image)){
+                unlink('uploads/movie/' .$movie->image);
+            }
+            $get_name_image = $get_image->getClientOriginalName();
+            $name_image = current(explode('.', $get_name_image));
+            $new_image = $name_image.rand(0,9999).'.'.$get_image->getClientOriginalExtension();
+            $get_image->move('uploads/movie/', $new_image);
+            $movie->image = $new_image;
+        }
+        $movie->save();
+        return redirect()->back();
     }
 
     /**
@@ -59,6 +141,11 @@ class MovieController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $movie = Movie::find($id);
+        if(!empty($movie->image)){
+            unlink('uploads/movie/' .$movie->image);
+        }
+        $movie->delete();
+        return redirect()->back();
     }
 }
--- resources/views/admin/movie/form.blade.php
+++ resources/views/admin/movie/form.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">Quản Lý Thể Loại</div>
+                <div class="card-header">Quản Lý PHIM</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,13 +14,20 @@
                         </div>
                     @endif
 
-                    @if(!isset($country))
+                    @if(!isset($movie))
                         <!-- Form Thêm Mới -->
-                        <form action="{{ route('country.store') }}" method="POST">
+                        <form action="{{ route('movie.store') }}" method="POST" enctype="multipart/form-data">
                             @csrf
                             <div class="form-group">
                                 <label for="title">Tiêu đề</label>
-                                <input type="text" value="{{isset($country) ? $country->title : ''}}" onkeyup="ChangeToSlug()" name="title" class="form-control" placeholder="Nhập vào dữ liệu..." id="slug">
+                                <input 
+                                    type="text" 
+                                    value="{{isset($movie) ? $movie->title: ''}}" 
+                                    onkeyup="ChangeToSlug()" 
+                                    name="title" 
+                                    class="form-control" 
+                                    placeholder="Nhập vào dữ liệu..." 
+                                    id="slug">
                                 <label for="slug">Slug</label>
                                 <input 
                                     type="slug" 
@@ -29,7 +36,7 @@
                                     class="form-control" 
                                     placeholder="Nhập vào dữ liệu..." 
                                     id="convert_slug"
-                                >   
+                                >
                                 <label for="description">Mô tả</label>
                                 <textarea type="text" name="description" class="form-control" placeholder="Nhập vào dữ liệu..." id="description"></textarea>
                                 <label for="status">Trạng thái:</label>
@@ -37,17 +44,37 @@
                                     <option value="1">Hiển thị</option>
                                     <option value="0">Không hiển thị</option>
                                 </select>
+                                <label>Danh mục</label>
+                                <select name="category_id" id="category_id" class="form-control" value="{{isset($movie) ? $movie->category: ''}}">
+                                    @foreach($category as $categoryId => $categoryTitle)
+                                        <option value="{{$categoryId}}">{{$categoryTitle}}</option>
+                                    @endforeach
+                                </select>
+                                <label>Thể loại</label>
+                                <select name="genre_id" id="genre_id" class="form-control" value="{{isset($movie) ? $movie->genre: ''}}">
+                                    @foreach($genre as $genreId => $genreTitle)
+                                        <option value="{{$genreId}}">{{$genreTitle}}</option>
+                                    @endforeach
+                                </select>
+                                <label>Quốc gia</label>
+                                <select name="country_id" id="country_id" class="form-control" value="{{isset($movie) ? $movie->country: ''}}">
+                                    @foreach($country as $countryId => $countryTitle)
+                                        <option value="{{$countryId}}">{{$countryTitle}}</option>
+                                    @endforeach
+                                </select>
+                                <label>Hình ảnh minh họa</label>
+                                <input type="file" name="image" id="image" placeholder="hình ảnh" class="form-control">
                             </div>
                             <button type="submit" class="btn btn-primary">Thêm dữ liệu</button>
                         </form>
                     @else
                         <!-- Form Sửa -->
-                        <form action="{{ route('country.update', $country->id) }}" method="POST">
+                        <form action="{{ route('movie.update', $movie->id) }}" method="POST" enctype="multipart/form-data">
                             @csrf
                             @method('PUT')
                             <div class="form-group">
                                 <label for="title">Tiêu đề</label>
-                                <input type="text" value="{{isset($country) ? $country->title : ''}} " onkeyup="ChangeToSlug()" name="title" class="form-control" placeholder="Nhập vào dữ liệu..." id="slug">
+                                <input type="text" value="{{isset($movie) ? $movie->title : ''}} " onkeyup="ChangeToSlug()" name="title" class="form-control" placeholder="Nhập vào dữ liệu..." id="slug">
                                 <label for="slug">Slug</label>
                                 <input 
                                     type="slug" 
@@ -56,16 +83,39 @@
                                     class="form-control" 
                                     placeholder="Nhập vào dữ liệu..." 
                                     id="convert_slug"
-                                >                                   
+                                >                                              
                                 <label for="description">Mô tả</label>
-                                <textarea type="text" name="description" class="form-control" placeholder="Nhập vào dữ liệu..." id="description">{{ $country->description }}</textarea>
+                                <textarea type="text" name="description" class="form-control" placeholder="Nhập vào dữ liệu..." id="description">{{ $movie->description }}</textarea>
                                 <label for="status">Trạng thái:</label>
                                 <select name="status" id="status" class="form-control">
-                                    <option value="1" {{ $country->status == 1 ? 'selected' : '' }}>Hiển thị</option>
-                                    <option value="0" {{ $country->status == 0 ? 'selected' : '' }}>Không hiển thị</option>
+                                    <option value="1" {{ $movie->status == 1 ? 'selected' : '' }}>Hiển thị</option>
+                                    <option value="0" {{ $movie->status == 0 ? 'selected' : '' }}>Không hiển thị</option>
                                 </select>
+                                <label>Danh mục</label>
+                                <select name="category_id" id="category_id" class="form-control" value="{{isset($movie) ? $movie->category: ''}}">
+                                    @foreach($category as $categoryId => $categoryTitle)
+                                        <option value="{{ $categoryId }}" {{ isset($movie) && $movie->category_id == $categoryId ? 'selected' : '' }}>{{ $categoryTitle }}</option>
+                                    @endforeach
+                                </select>
+                                <label>Thể loại</label>
+                                <select name="genre_id" id="genre_id" class="form-control" value="{{isset($movie) ? $movie->genre: ''}}">
+                                    @foreach($genre as $genreId => $genreTitle)
+                                        <option value="{{ $genreId }}" {{ isset($movie) && $movie->genre_id == $genreId ? 'selected' : '' }}>{{ $genreTitle }}</option>
+                                    @endforeach
+                                </select>
+                                <label>Quốc gia</label>
+                                <select name="country_id" id="country_id" class="form-control" value="{{isset($movie) ? $movie->country: ''}}">
+                                    @foreach($country as $countryId => $countryTitle)
+                                        <option value="{{ $countryId }}" {{ isset($movie) && $movie->country_id == $countryId ? 'selected' : '' }}>{{ $countryTitle }}</option>
+                                    @endforeach
+                                </select>
+                                <label>Hình ảnh</label>
+                                @if($movie)
+                                <input type="file" name="image" id="image" placeholder="hình ảnh" class="form-control">
+                                    <img src="{{asset('uploads/movie/'.$movie->image)}}" sizes="40%">
+                                @endif
                             </div>
-                            @if(!isset($country))
+                            @if(!isset($movie))
                                 <button type="submit" class="btn btn-primary">Lưu thay đổi</button>
                             @else
                                 <button type="submit" class="btn btn-success">Cập nhật</button>
@@ -82,9 +132,13 @@
             <tr>
                 <th scope="col">STT</th>
                 <th scope="col">Tiêu đề</th>
+                <th scope="col">Hình ảnh</th>
                 <th scope="col">Mô tả</th>
                 <th scope="col">Slug</th>
                 <th scope="col">Trạng thái</th>
+                <th scope="col">Danh mục</th>
+                <th scope="col">Thể loại</th>
+                <th scope="col">Quốc gia</th>
                 <th scope="col">Quản lý</th>
             </tr>
         </thead>
@@ -93,21 +147,26 @@
             <tr>
                 <th scope="row">{{ $key + 1 }}</th>
                 <td>{{ $cate->title }}</td>
+                <td><img src="{{asset('uploads/movie/'.$cate->image)}}" width="150" height="200"></td>
                 <td>{{ $cate->description }}</td>
                 <td>{{ $cate->slug }}</td>
                 <td>
                     {{ $cate->status ? 'Hiển thị' : 'Không hiển thị' }}
                 </td>
+                <td>{{ $cate->category->title }}</td>
+                <td>{{ $cate->genre->title }}</td>
+                <td>{{ $cate->country->title }}</td>
+
                 <td>
                     <!-- Nút Xóa -->
-                    <form id="delete-form-{{ $cate->id }}" action="{{ route('country.destroy', $cate) }}" method="POST">
+                    <form id="delete-form-{{ $cate->id }}" action="{{ route('movie.destroy', $cate) }}" method="POST">
                         @csrf
                         @method('DELETE')
                         <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#confirmDeleteModal{{ $cate->id }}">Xóa</button>
                     </form>
 
                     <!-- Nút Sửa -->
-                    <a href="{{ route('country.edit', $cate->id) }}">
+                    <a href="{{ route('movie.edit', $cate->id) }}">
                         <button type="button" class="btn btn-warning">Sửa</button>
                     </a>
                 </td>
